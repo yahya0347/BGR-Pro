4e26a2e71fcecbb749a72b32395d053a5bd0965d
MACBOOK AIR M1 <macbookairm1@MACBOOKs-MacBook-Air.local>
2026-06-15 02:51:22 +0500

    fix: add flex-direction: column to landing pages to fix vertical layout and scroll lock bug, bump asset versions to v=22

--- app.js
+++ app.js
@@ -1287,7 +1287,7 @@ function runWatermarkInpaint() {
     state.inpaintWorker.terminate();
     state.inpaintWorker = null;
   }
-  state.inpaintWorker = new Worker('inpaint-worker.js?v=21');
+  state.inpaintWorker = new Worker('inpaint-worker.js?v=22');
   
   // Post data to worker
   state.inpaintWorker.postMessage({
